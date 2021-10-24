fa562165c8471ac282299a4ad6cbb0374a4a810e
4lexRossi <devalexrossi@gmail.com>
2021-10-24 20:47:54 -0300

    Creating admin app

--- apps/admin/src/app/app.component.ts
+++ apps/admin/src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ng-shop-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  title = 'admin';
 }
