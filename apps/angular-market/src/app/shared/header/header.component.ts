a4237ccd6dc4a1bb8a47271b52b3a79b006d4849
4lexRossi <devalexrossi@gmail.com>
2021-10-28 22:55:36 -0300

    Minor changes

--- apps/angular-market/src/app/shared/header/header.component.ts
+++ apps/angular-market/src/app/shared/header/header.component.ts
@@ -1,17 +1,12 @@
-/* eslint-disable @angular-eslint/no-empty-lifecycle-method */
-/* eslint-disable @typescript-eslint/no-empty-function */
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'ng-shop-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
